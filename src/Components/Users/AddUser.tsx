cb3def50658ce58f83bb5d48fc0d67f6dd30bf86
Federico Amormino <federico.amormino@gmail.com>
2023-01-18 12:19:53 -0300

    Adding Card

--- src/Components/Users/AddUser.tsx
+++ src/Components/Users/AddUser.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import Card from '../UI/Card'
 
-const AddUser = (props) => {
+const AddUser = () => {
 
     const addUserHandle = (event) => {
         event.preventDefault()
     }
 
     return (
-        <form onSubmit={addUserHandle}>
-            <label htmlFor="userName">User</label>
-            <input id="userName" type="text" />
-            <label htmlFor="age">Age</label>
-            <input id="age" type="number" />
-            <button type='submit'>Add</button>
-        </form>
+        <Card>
+            <form onSubmit={addUserHandle}>
+                <label htmlFor="userName">User</label>
+                <input id="userName" type="text" />
+                <label htmlFor="age">Age</label>
+                <input id="age" type="number" />
+                <button type='submit'>Add</button>
+            </form>
+        </Card>
     )
 }
 
