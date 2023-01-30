606599704166b59fb82ca8c42c3e0b42f98dddef
Federico Amormino <federico.amormino@gmail.com>
2023-01-30 18:12:09 -0300

    adding ReactDOM.createPortal

--- src/Components/UI/ErrorModal.jsx
+++ src/Components/UI/ErrorModal.jsx
@@ -1,23 +1,44 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 import classes from "./ErrorModal.module.css";
 
+const BackDrop = (props) => (
+    <div className={classes.backdrop} onClick={props.onConfirm} />
+);
+
+const Modal = (props) => {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <main className={classes.content}>
+                <div>{props.message}</div>
+            </main>
+            <footer className={classes.actions}>
+                <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+        </Card>
+    );
+};
+
 const ErrorModal = (props) => {
     return (
         <React.Fragment>
-            <div className={classes.backdrop} onClick={props.onConfirm} />
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <main className={classes.content}>
-                    <div>{props.message}</div>
-                </main>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
+            {ReactDOM.createPortal(
+                <BackDrop onConfirm={props.onConfirm} />,
+                document.getElementById("backdrop")
+            )}
+            {ReactDOM.createPortal(
+                <Modal
+                    title={props.title}
+                    message={props.message}
+                    onConfirm={props.onConfirm}
+                />,
+                document.getElementById("modal")
+            )}
         </React.Fragment>
     );
 };
